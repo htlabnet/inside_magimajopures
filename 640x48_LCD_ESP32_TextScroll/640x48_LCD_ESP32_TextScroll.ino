40905bdc2d68fd21ee14dd7eab7ad8821737c335
lovyan03 <42724151+lovyan03@users.noreply.github.com>
2021-01-23 22:12:00 +0900

    コメント調整

--- 640x48_LCD_ESP32_TextScroll/640x48_LCD_ESP32_TextScroll.ino
+++ 640x48_LCD_ESP32_TextScroll/640x48_LCD_ESP32_TextScroll.ino
@@ -25,7 +25,7 @@
 //  21  - LED-  [to GND]
 //  22  - GND   [to GND]
 //
-
+// ※ PSRAMを使用する場合は GPIO 16 , 17 は使用できないので注意。
 
 #include <LovyanGFX.hpp>
 
@@ -47,6 +47,7 @@ struct LGFX_Config {
 constexpr char text[] = "マジョカアイリス！マジマジョマジカル！あなたも変身！魔法×戦士 マジマジョピュアーズ！きらめく魔法でピュアライズ！ホンモノガーネット！ビッグアクアマリン！レインボーミルキーファンタジー！遊びがいっぱい！マジョカアイリス！";
 constexpr uint32_t textlen = sizeof(text) / sizeof(text[0]);
 constexpr int32_t panel_width = 640;
+constexpr int32_t scroll_step = 1;
 
 static lgfx::LGFX_PARALLEL<LGFX_Config> lcd;
 
@@ -61,6 +62,7 @@ inline void lcd_buffer_write()
   lcd.setAddrWindow(0, 0, 320, 96);
   auto b = (lgfx::swap565_t*)buf.getBuffer();
   auto bufwidth = buf.width();
+  // バッファ幅が640より1文字分長いので、まとめて出力するとズレるため、1ラインずつループで出力
   for (int i = 0; i < 48; i++)
   {
     lcd.writePixels(&b[bufwidth * i], panel_width);
@@ -94,7 +96,7 @@ void setup(void)
   lcd.setRotation(0); // 0 or 2
 
   buf.setColorDepth(16);
-  buf.createSprite(panel_width + 40, 48);
+  buf.createSprite(panel_width + 40, 48);  // バッファ幅はパネル幅より1文字分広くしておく
   buf.setFont(&fonts::lgfxJapanMinchoP_40);
   buf.setTextWrap(false);
 }
@@ -103,15 +105,15 @@ void setup(void)
 void loop(void)
 {
   static size_t textpos = 0;
-  int32_t cursor_x = buf.getCursorX() - 1;   // 現在のカーソル位置を取得し、1ドット左に移動
+  int32_t cursor_x = buf.getCursorX() - scroll_step;   // 現在のカーソル位置を取得し、スクロール量の分だけ左に移動
   if (cursor_x <= 0) // カーソル位置が左端に到達している場合は一周したと判断
   {
     textpos = 0;            // 文字列の読取り位置をリセット
     cursor_x = panel_width; // 新たな文字が画面右端に描画されるようにカーソル位置を変更
   }
 
-  buf.setCursor(cursor_x, 0); // カーソル位置を更新
-  buf.scroll(-1, 0);          // キャンバスの内容を1ドット左にスクロール
+  buf.setCursor(cursor_x, 0);  // カーソル位置を更新
+  buf.scroll(-scroll_step, 0); // バッファ内容を左にスクロール
   while (textpos < textlen && cursor_x <= panel_width) // 画面右端に文字が書けるか判定
   {
     buf.print(text[textpos++]);   // 1バイトずつ出力 (マルチバイト文字でもこの処理で動作します)
